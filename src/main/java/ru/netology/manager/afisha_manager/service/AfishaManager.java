81b5ee527a5cd7f97c0b7b0c9180bab05cdb0bf7
Dmitrii Selin <120986503+Dimitrius199@users.noreply.github.com>
2023-02-27 23:27:35 +0500

    First commit

---
+++ src/main/java/ru/netology/manager/afisha_manager/service/AfishaManager.java
@@ -0,0 +1,41 @@
+package ru.netology.manager.afisha_manager.service;
+
+public class AfishaManager {
+    private String[] films = new String[0];
+    private int limit;
+
+    public AfishaManager() {
+        this.limit = 10;
+    }
+
+    public AfishaManager(int limit) {
+        this.limit = limit;
+    }
+
+    public String[] addFilm(String newFilm) {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = newFilm;
+        films = tmp;
+        return films;
+    }
+
+    //
+    public String[] findAll() {
+        return films;
+    }
+
+    public String[] findLast() {
+        int arrayMassive = limit;
+        if (limit > films.length) {
+            arrayMassive = films.length;
+        }
+        String[] reversed = new String[arrayMassive];
+        for (int i = 0; i < reversed.length; i++) {
+            reversed[i] = films[films.length - 1 - i];
+        }
+        return reversed;
+    }
+}
